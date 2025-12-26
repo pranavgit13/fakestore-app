e5dcb12c57ff7256fe76df994f8697b782d31a65
pranavgit13 <128689310+pranavgit13@users.noreply.github.com>
2025-12-26 14:00:52 +0530

    indexjs fixed

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { CartProvider } from "./context/CartContext";
 import { ThemeProvider } from "./context/ThemeContext";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThemeProvider>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ThemeProvider>
+  </BrowserRouter>
 );
